c4467063f5f956a7f0ebc21353d3dfb09e842ee8
David Fischer <dfischer@fischerltd.com>
2025-07-19 14:09:17 -0400

    commit

--- backend/app/routes/app.routes.js
+++ backend/app/routes/app.routes.js
@@ -27,10 +27,7 @@ const controllers = {
   pofromvendor: require("../controllers/pofromvendor.controller.js"),
   checkSearch: require("../controllers/lacp441s.controller.js"),
   ppur410lxs: require("../controllers/ppur410lxs.controller.js"),
-  // s3000EmpSrch: require("../controllers/s3000EmpSrch.controller.js"),
-  // s3000ShowPayments: require("../controllers/s3000ShowPayments.controller.js"),
-  // s3000DisplayPayment: require("../controllers/s3000DisplayPayment.controller.js")  // ✅ Import the new controller - COMMENTED OUT UNTIL CONTROLLERS ARE CREATED
-};
+  };
 
 // 🚀 Log missing controllers for debugging
 Object.entries(controllers).forEach(([name, controller]) => {
@@ -98,14 +95,6 @@ safeRoute("get", "/lacp441s/", controllers.lacp441s.findAll, "lacp441s.findAll")
 safeRoute("get", "/checksearch/", controllers.lacp441s.checkSearch, "lacp441s.checkSearch");
 safeRoute("get", "/ppur301s/", controllers.ppur301s.findAll, "ppur301s.findAll");
 safeRoute("get", "/ppur301s/:id", controllers.ppur301s.findOne, "ppur301s.findOne");
-
-// ✅ Employee Search Routes - COMMENTED OUT UNTIL S3000 CONTROLLERS ARE CREATED
-// GET /s3000EmpSrch/:emp_num - Fetch employee by number (returns 404 if not found)
-// safeRoute("get", "/s3000EmpSrch/:emp_num", controllers.s3000EmpSrch.findOne, "s3000EmpSrch.findOne");
-// GET /s3000EmpSrch?name=lastname - Fetch employee(s) by last name (404 if none found)
-// safeRoute("get", "/s3000EmpSrch", controllers.s3000EmpSrch.findAll, "s3000EmpSrch.findAll");
-// safeRoute("get", "/s3000ShowPayments/:employeeNumber", controllers.s3000ShowPayments.findAll, "s3000ShowPayments.findAll");
-// safeRoute("get", "/s3000DisplayPayment/:chknum/:chkdate", controllers.s3000DisplayPayment.findAllByCheck, "s3000DisplayPayment.findAllByCheck");
 
 // ✅ Employee Routes
 safeRoute("get", "/", controllers.employees.findAll, "employees.findAll");
